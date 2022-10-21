b9df97eb58a08b843b061b0983324ee06ccba8b9
Erick Perez Rayo <servicios.terceros@interaction.cr>
2022-10-21 12:12:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="assets/main.css">
-		<link rel="stylesheet" href="assets/bim-registro.css">
+		
 		<link rel="stylesheet" href="assets/fonts.css">
 		<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
 
